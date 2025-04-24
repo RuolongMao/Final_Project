ebc4fcbd82e5e085d837873f6d7ac3bbabfa7300
unknown <marcomao0719@gmail.com>
2025-04-24 11:39:42 -0400

    Implementation v1 completed

--- src/main/scala/final_project/clustering.scala
+++ src/main/scala/final_project/clustering.scala
@@ -16,22 +16,55 @@ object main {
     def pivotClustering(graph: Graph[Int, Int]): VertexRDD[Long] = {
         val rand = new Random()
 
+        var currentVertices = graph.vertices
+        var currentEdges = graph.edges
         // random permutation
-        val piValues = graph.vertices.mapValues(_ => rand.nextDouble())
+        val piValues = currentVertices.mapValues(_ => rand.nextDouble())
+        var clustered = sc.emptyRDD[(VertexId, Long)]
+        var clusterId = 100
 
         while (currentVertices.count() > 0) {
             // select pivot with smallest pi
             val minNeighborPi = Graph(currentVertices, currentEdges)
                 .aggregateMessages[Double](
-                triplet => {
-                    if (triplet.srcAttr > triplet.dstAttr)
-                    triplet.sendToSrc(triplet.dstAttr)
-                    else if (triplet.dstAttr > triplet.srcAttr)
-                    triplet.sendToDst(triplet.srcAttr)
-                },
-                (a, b) => math.min(a, b)
+                    triplet => {
+                        if (triplet.srcAttr > triplet.dstAttr)
+                        triplet.sendToSrc(triplet.dstAttr)
+                        else if (triplet.dstAttr > triplet.srcAttr)
+                        triplet.sendToDst(triplet.srcAttr)
+                    },
+                    (a, b) => math.min(a, b)
                 )
+            
+            val pivots = currentVertices.leftOuterJoin(minNeighborPi).filter { case (_, (pi, min)) => min.isEmpty || pi < min.get }.mapValues(_ => clusterId)
+
+
+            // Every node joins the pivots with the smallest pi
+            val pivotGraph = Graph(pivots, currentEdges)
+
+            val assignments = pivotGraph.aggregateMessages[(VertexId, Double)](
+                triplet => {
+                triplet.sendToDst((triplet.srcId, triplet.srcAttr))
+                triplet.sendToSrc((triplet.dstId, triplet.dstAttr))
+                },
+                (a, b) => if (a._2 < b._2) a else b
+            ).mapValues(_ => clusterId)
+
+            val newCluster = pivots.union(assignments)
+
+            // Remove clustered nodes
+            val clusteredIds = newCluster.map(_._1).collect().toSet
+            val bcClustered = sc.broadcast(clusteredIds)
+
+            currentVertices = currentVertices.filter { case (vid, _) => !bcClustered.value.contains(vid) }
+            currentEdges = currentEdges.filter { e =>
+                !bcClustered.value.contains(e.srcId) && !bcClustered.value.contains(e.dstId)
+            }
+
+            clustered = clustered.union(newCluster)
+            clusterId += 100 
         }
+        VertexRDD(clustered)
     }
 
     def main(args: Array[String]): Unit = {
