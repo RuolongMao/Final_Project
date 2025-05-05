4f37ade3dce97d71bb23f5a8b028c337fe0fdcf5
RuolongMao <94766074+RuolongMao@users.noreply.github.com>
2025-05-04 20:45:00 -0400

    Add files via upload

--- src/main/scala/final_project/clustering_verifier.scala
+++ src/main/scala/final_project/clustering_verifier.scala
@@ -14,6 +14,7 @@ object clustering_verifier{
     val conf = new SparkConf().setAppName("verifier")
     val sc = new SparkContext(conf)
     val spark = SparkSession.builder.config(conf).getOrCreate()
+/* You can either use sc or spark */
 
     sc.setLogLevel("ERROR")
 
@@ -30,9 +31,12 @@ object clustering_verifier{
         Edge(x(1).toLong, x(0).toLong, 1)
     }
 
+
+
     case class VertexData(data1: Long, data2: Long)
       
     val graph_edges = sc.textFile(args(0)).map(line_to_canonical_edge)
+
     val graph_vertices = sc.textFile(args(1)).map(line => line.split(",")).map( parts => (parts(0).toLong, parts(1).toLong) )
 
     if(graph_vertices.keys.distinct.count != graph_vertices.count){
@@ -46,20 +50,41 @@ object clustering_verifier{
       sys.exit(1)      
     }
 
-    // Create a graph with cluster assignments
-    val graph = Graph(graph_vertices, graph_edges)
-    
-    // Create a broadcast variable for vertex cluster assignments
-    val vertexClusters = graph.vertices.collectAsMap()
-    val bcVertexClusters = sc.broadcast(vertexClusters)
+    val graph_vertices2 = graph_vertices.map(x => (x._2,x._1))
+    val graph_vertices3 = graph_vertices2.join(graph_vertices.map(x => (x._2, 1L)).reduceByKey(_+_)).map({case (a,x) => (x._1,(a,x._2))})
 
-    // Calculate disagreement by counting edges between different clusters
-    val disagreement = graph.edges.filter { edge =>
-      val srcCluster = bcVertexClusters.value.getOrElse(edge.srcId, -1L)
-      val dstCluster = bcVertexClusters.value.getOrElse(edge.dstId, -1L)
-      srcCluster != dstCluster
-    }.count()
+    val graph2: Graph[Tuple2[Long, Long], Int]  = Graph(graph_vertices3, graph_edges, (0L,0L))
 
-    println("The clustering has a disagreement of: " + disagreement)
+
+    val degrees = graph2.degrees
+    val graph3 = graph2.outerJoinVertices(degrees){
+      case (id, (a,b), degOpt) => degOpt match {
+        case Some(deg) => (a, b ,deg)
+        case None => (a,b,0)
+      }
+    }
+    val graph4 = graph3.aggregateMessages[Int](e=>{
+        if(e.srcAttr._1 == e.dstAttr._1){
+          e.sendToDst(1)
+          e.sendToSrc(1)
+        }
+      }, _+_)
+
+    val graph5 = graph3.outerJoinVertices(graph4){
+        (id, x, newOpt) => newOpt match {
+        case Some(newAttr) => (x._1,x._2,x._3,newAttr)
+        case None => (x._1,x._2,x._3,0)
+    }  }
+    val graph6 = graph5.mapVertices{ (ID,ver) => 
+      {
+        val component_size = ver._2
+        val degree = ver._3
+        val same_component_agreement = ver._4
+        (component_size - 1 - same_component_agreement) + (degree - same_component_agreement)
+      }
+    }
+//    graph5.vertices.collect().foreach(println)
+    val ans = graph6.vertices.aggregate(0L)({case (x, v) => x + v._2 }, (x,y) => x+y) 
+    println("The clustering has a disagreement of: "+(ans/2))
   }
 }
