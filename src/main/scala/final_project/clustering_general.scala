0e6bc9d25bfde1aa74741de0697396af8f491ef1
mokho1 <143531279+mokho1@users.noreply.github.com>
2025-05-04 21:47:37 -0400

    Update clustering_general.scala
    clean comments

--- src/main/scala/final_project/clustering_general.scala
+++ src/main/scala/final_project/clustering_general.scala
@@ -16,17 +16,14 @@ object clustering_general {
     Logger.getLogger("org.spark-project").setLevel(Level.WARN)
 
     def pivotClustering(graph: Graph[Int, Int], sc: SparkContext): Graph[Long, Int] = {
-        // Initialize random number generator and assign random values to vertices
         val rand = new Random(System.currentTimeMillis())
         val piValues = graph.vertices.mapValues(_ => rand.nextDouble())
         
-        // Create initial graph with random values
         var g = graph.outerJoinVertices(piValues) {
             case (_, _, Some(pi)) => pi
             case (_, _, None)     => Double.MaxValue
         }
 
-        // Initialize variables for clustering process
         var currentVertices = g.vertices
         var currentEdges = g.edges
         var clustered = sc.emptyRDD[(VertexId, Long)]
@@ -34,7 +31,6 @@ object clustering_general {
         
         println(s"Initial vertex count: ${currentVertices.count()}")
         
-        // Main clustering loop
         var iteration = 0
         val piValuesMap = piValues.persist(StorageLevel.MEMORY_AND_DISK)
         
@@ -46,54 +42,45 @@ object clustering_general {
             println(s"Iteration $iteration, Remaining vertices: $remainingCount")
             println("----------------------------------------------------")
 
-            // Get neighbor information for each vertex
             val neighborIds = currentEdges.flatMap { e =>
                 if (e.srcId != e.dstId) Seq((e.srcId, e.dstId), (e.dstId, e.srcId)) else Seq.empty
             }.groupByKey()
 
-            // Join vertices with their pi values and neighbors
             val vertexWithNeighborPis = currentVertices.join(piValuesMap).leftOuterJoin(neighborIds).mapValues {
                 case ((value, piVal), maybeNeighbors) => 
                     (piVal, maybeNeighbors.getOrElse(Iterable.empty))
             }
 
-            // Create broadcast variable for neighbor pi values
             val neighborPiMap = piValuesMap.collectAsMap()
             val bcNeighborPiMap = sc.broadcast(neighborPiMap)
 
-            // Find local minimum candidates
             val localMinCandidates = vertexWithNeighborPis.filter { case (vid, (piVal, neighbors)) =>
                 if (neighbors.isEmpty) {
                     true
                 } else {
                     neighbors.forall { nid =>
                         val neighborPiVal = bcNeighborPiMap.value.getOrElse(nid, Double.MaxValue)
-                        piVal <= neighborPiVal
+                        piVal < neighborPiVal
                     }
                 }
             }
 
-            // Assign cluster IDs to pivots and their neighbors
             val pivotAndNeighborAssignments = localMinCandidates.flatMap { case (pivotId, (_, neighbors)) =>
                 val thisPivotClusterId = clusterId
                 clusterId += 1
                 (pivotId +: neighbors.toSeq).map(vid => (vid, thisPivotClusterId))
             }
 
-            // Resolve conflicts by keeping lower cluster ID
             val pivotAndNeighborsRDD = pivotAndNeighborAssignments.reduceByKey((a, b) => math.min(a, b))
             val uniqueClustersThisIteration = pivotAndNeighborsRDD.map(_._2).distinct().count()
 
-            // Update clustered vertices
             clustered = clustered.union(pivotAndNeighborsRDD)
 
-            // Remove clustered vertices from current graph
             val clusteredIdsRDD = pivotAndNeighborsRDD.map(_._1)
             val clusteredIdsSet = clusteredIdsRDD.distinct().map((_, null))
             
             currentVertices = VertexRDD(currentVertices.subtractByKey(clusteredIdsSet))
             
-            // Update edges to remove those connected to clustered vertices
             val currentVerticesSet = currentVertices.map(_._1).collect().toSet
             val bcCurrentVertices = sc.broadcast(currentVerticesSet)
             
@@ -114,11 +101,9 @@ object clustering_general {
         println(s"Clustering complete. Total clusters: ${clusterId - 1}")
         println("**----------------------------------------------------**")
         
-        // Create final graph with cluster assignments
         Graph(clustered, graph.edges)
     }
 
-    // Break helper
     def break = throw new BreakException()
     class BreakException extends RuntimeException
 
